4cd251cd3c0e589e4517095f6fbe760566239bb0
Greg Payne <greg.payne@gmail.com>
2014-03-20 08:48:43 -0500

    Added additional memory test.

--- src/Tests/PerformanceTest.php
+++ src/Tests/PerformanceTest.php
@@ -23,7 +23,24 @@ class PerformanceTest extends \PHPUnit_Framework_TestCase {
     }
     $post = memory_get_usage(FALSE);
     $memory_per_reference = round(($post - $pre) / $count);
-    $this->assertLessThanOrEqual(21394, $memory_per_reference, "Early tests only show 21K per additional iterator in context.");
+    // This test is targeted to PHP 5.3
+    // 5.4 and 5.5 both use 25% less memory
+    $this->assertLessThanOrEqual(21400, $memory_per_reference, "Early tests only show 21K per additional iterator in context.");
+
+    // Document how much memory is used when additional iterators are instantiated.
+    $quip = Quip::load(__DIR__ . '/Resources/XmlBasicList.xml', 0, TRUE);
+    $count = 1000;
+    $v = array();
+    $pre = memory_get_usage(FALSE);
+    for ($i = 0; $i < $count; ++$i) {
+      $v[] = $quip->xpath("//original");
+    }
+    $post = memory_get_usage(FALSE);
+    $memory_per_reference = round(($post - $pre) / $count);
+    var_dump($memory_per_reference);
+    // This test is targeted to PHP 5.3
+    // 5.4 and 5.5 both use 25% less memory
+    $this->assertLessThanOrEqual(2250, $memory_per_reference, "Early tests only show 2.2K per additional 1-item iterator in context.");
   }
 
 }
