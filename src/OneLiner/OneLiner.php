dd749246ce7c6a49ab208d91ed1bcc166fe81223
Greg Payne <greg.payne@gmail.com>
2015-08-20 10:04:44 -0500

    Add OneLiner namespace for quick markup generation.

---
+++ src/OneLiner/OneLiner.php
@@ -0,0 +1,62 @@
+<?php
+/*
+ * This file is part of the QuipXml package.
+ *
+ * (c) Greg Payne
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace QuipXml\OneLiner;
+class OneLiner {
+  static public function wrap($wrapper, $content) {
+    // Catch uninteresting cases quickly.
+    if (!isset($wrapper) || $wrapper === '') {
+      return $content;
+    }
+
+    // Just a tag name.
+    if (preg_match('@^[a-z0-9]+$@si', $wrapper)) {
+      $output = "<$wrapper>$content</$wrapper>";
+      return $output;
+    }
+
+    // Handle opening tags.
+    if (strpos($wrapper, '<') !== FALSE) {
+      $output = $wrapper . $content;
+      $parts = explode('<', $wrapper);
+      array_shift($parts);
+      $closed = array();
+      foreach (array_reverse($parts) as $part) {
+        if ($part{0} === '/') {
+          if (preg_match('@^/([^>]*)>@s', $part, $arr)) {
+            $tag = trim($arr[1]);
+            if (!isset($closed[$tag])) {
+              $closed[$tag] = 1;
+            }
+            else {
+              ++$closed[$tag];
+            }
+          }
+        }
+        else {
+          if (preg_match('@^([^>\s]*)[\s>]@s', $part, $arr)) {
+            $tag = trim($arr[1]);
+            if (isset($closed[$tag]) && $closed[$tag] > 0) {
+              --$closed[$tag];
+            }
+            else {
+              $output .= "</$tag>";
+            }
+          }
+        }
+      }
+      return $output;
+    }
+
+    // If nothing works, then simply prepend the wrapper.
+    $output = $wrapper . $content;
+  }
+
+}
