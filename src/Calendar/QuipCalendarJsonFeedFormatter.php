b70e9e57a73a8e6cfe2a9158a7634f082fe50493
Greg Payne <greg.payne@gmail.com>
2016-02-23 09:12:25 -0600

    Add support for 'location' element

--- src/Calendar/QuipCalendarJsonFeedFormatter.php
+++ src/Calendar/QuipCalendarJsonFeedFormatter.php
@@ -55,60 +55,20 @@ class QuipCalendarJsonFeedFormatter extends QuipCalendarIcsFormatter {
     $event['start'] = $this->getDateTime($vevent->dtstart->html(), $vevent);
     $event['end'] = $this->getDateTime($vevent->dtend->html(), $vevent);
     $event['uid'] = $vevent->uid->html();
-
-    //     // Add the attributes
-    //     $attrs = '';
-    //     foreach ($xml->attributes() as $k => $v) {
-    //       $k = strtoupper($k);
-    //       $attrs .= ";$k=$v";
-    //     }
-
-    //     // Add the children.
-    //     $number_children = 0;
-    //     foreach ($xml->children() as $child) {
-    //       ++$number_children;
-    //       $output .= $this->getFormattedRecursiveIterator($child, $child->getName());
-    //     }
-
-    //     // Wrap in the tag.
-    //     if (isset($tag) && strlen($tag) > 0) {
-    //       $tag = strtoupper($tag);
-    //       if ($number_children) {
-    //         $output = "BEGIN:$tag$lf{$output}END:$tag$lf";
-    //       }
-    //       else {
-    //         $value = strtr($xml->html(), $cleantr);
-    //         if (substr($tag, 0, 2) === 'DT' && $attrs === '') {
-    //           // Get the timezone.
-    //           $tz = NULL;
-    //           $tz_name = trim($xml->xpath('//x-wr-timezone')->html());
-    //           if ($tz_name !== '') {
-    //             $tz = new \DateTimeZone($tz_name);
-    //             $utc = new \DateTimeZone("UTC");
-    //           }
-
-    //           // Apply the timezone shift to use UTC.
-    //           if (substr($value, -1) !== 'Z' && isset($tz)) {
-    //             $dt = new \DateTime($value, $tz);
-    //             $dt->setTimezone($utc);
-    //             $value = $dt->format('Ymd\THis\Z');
-    //           }
-    //         }
-    //         $line = "$tag$attrs:" . $value;
-    //         if (strlen($line) <= 75) {
-    //           $output = $line . $lf;
-    //         }
-    //         else {
-    //           $output = substr($line, 0, 75) . $lf;
-    //           $line = substr($line, 75);
-    //           while (strlen($line) > 74) {
-    //             $output .= ' ' . substr($line, 0, 74) . $lf;
-    //             $line = substr($line, 74);
-    //           }
-    //           $output .= " $line$lf";
-    //         }
-    //       }
-    //     }
+    if ($vevent->location) {
+      $event['location'] = array();
+      $event['location']['data'] = trim($vevent->location->html());
+      foreach ($vevent->location->attributes() as $k => $v) {
+        $k = strtolower($k);
+        if (substr($k, 0, 2) === 'x-') {
+          $k = substr($k, 2);
+        }
+        $event['location'][$k] = trim($v);
+      }
+      if ($event['location']['data'] === '') {
+        unset($event['location']);
+      }
+    }
     return $event;
   }
 
